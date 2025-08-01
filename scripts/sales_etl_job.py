38dc78a4ca1ff19e456dbbf71d68fc4194fdae6f
OtavioCsar <80010479+OtavioCsar@users.noreply.github.com>
2025-08-01 17:50:41 -0300

    Update sales_etl_job.py

--- scripts/sales_etl_job.py
+++ scripts/sales_etl_job.py
@@ -1,4 +1,4 @@
-from awsglue.transform import *
+from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
@@ -13,23 +13,24 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Lê os dados de vendas do Glue Data Catalog
-sales_data = glueContext.create_dynamic_frame.from_catalog(
+# Lê dados do Glue Data Catalog (tabela criada por crawler)
+sales_df = glueContext.create_dynamic_frame.from_catalog(
     database="sales_db",
-    table_name="sales_table",
+    table_name="sales_data"
 ).toDF()
 
 # Transformação: Calcula o total de vendas (quantity * price) por região
-sales_df = sales_df.withColumn("total_sales", sales.df["quantity"] * sales_df["price"])
-aggregated_df = sales_df.groupBy("region").agg({"total_sales": "sum"})
+sales_df = sales_df.withColumn("total_sale", sales_df.quantity * sales_df.price)
+aggregated_df = sales_df.groupBy("region").agg({"total_sale": "sum"})
 
 # Salva o resultado em Parquet no S3
 glueContext.write_dynamic_frame.from_df(
     aggregated_df,
     glueContext,
-    "processed_sales",
+    "processed_sales"
 ).write_parquet(
-    "s3://processed-sales-data-<test>-region/sales_parquet/"
+    "s3://processed-sales-data-<seu-nome>/sales_parquet/"
 )
+
 
 job.commit()
